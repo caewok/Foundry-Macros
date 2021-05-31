2758178420a289cdf75655fc9532a7640aebe39a
Michael Enion <caewok_2000@yahoo.com>
2021-05-31 14:41:23 -0700

    Add functions to locate visible tokens
    Find all visible tokens for a selected token.

--- helper_functions.js
+++ helper_functions.js
@@ -44,6 +44,59 @@ function RetrieveActorsForTokens(token_array) {
   return token_array.map(t => t.actor)
 }
 
+/**
+ * Retrieve visible tokens
+ * For GM, all will be visible unless 1 or more tokens are selected.
+ * Combined vision for all tokens selected.
+ */
+function RetrieveVisibleTokens() {
+  return canvas.tokens.children[0].children.filter(c => c.visible);
+}
+
+/**
+ * Retrieve actor by id.
+ */
+function RetrieveActor(id) { return game.actors.get(id); }
+
+/**
+ * Retrieve token by id.
+ */
+function RetrieveToken(id) { return canvas.tokens.get(id); }
+
+/**
+ * Retrieve visible tokens for token
+ * To accomplish this, the token is temporarily selected.
+ * Will fail if GM has Less Fog or similar module and has 
+ * enabled see all tokens. 
+ */
+function RetrieveVisibleTokensForToken(the_token) {
+  const selected_tokens = RetrieveSelectedTokens()
+  the_token.control({releaseOthers: true});
+  the_token.refresh()
+  
+  const visible_tokens = RetrieveVisibleTokens()
+  
+  the_token.release();
+  the_token.refresh();
+  
+  // reselect all
+  selected_tokens.forEach(t => {
+    t.control({releaseOthers: false});
+    t.refresh();
+  });
+  
+  return visible_tokens;
+}
+
+/**
+ * Wait for given number of milliseconds.
+ * Example: await wait(1000)
+ */
+async function wait(ms) {
+  return new Promise(resolve => { setTimeout(resolve, ms); });
+}
+
+
 
 /**
  * Test for blank or empty string
