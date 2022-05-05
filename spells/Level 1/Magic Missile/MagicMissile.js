da41d84acf49df939bbd1f81c122f299c9fdc30c
Michael Enion <caewok_2000@yahoo.com>
2022-05-05 08:49:56 -0700

    Update Magic Missile for Foundry v9
    Sequencer v2
    Add the JSON.
    Consolidate the separate missile animation macro with this one.

--- spells/Level 1/Magic Missile/MagicMissile.js
+++ spells/Level 1/Magic Missile/MagicMissile.js
@@ -1,125 +1,92 @@
+/* Magic Missile midi-qol on-use macro
+• Add name of the macro to the Magic Missile On-Use field.
+• Select "After active effects"
+• Turn off Automated animations for the spell if applicable.
+
+Required modules:
+• Midi-qol
+• JB2A patreon. (If not patreon, change COLOR below to available color.)
+• Sequencer
+
+If one token is targeted, fires missiles at the target. If more than one target, first
+presents a dialog allowing the caster to select how many missiles should fire at each
+target.
+
+Applies 5e RAW rule that caster rolls a single 1d4 + 1 and uses that damage for each missile.
+
+Change COLOR variable below if you prefer a specific color (default: random).
+Change DAMAGE_TYPE variable below if you prefer a different damage type (default: FORCE).
+
+/*
+
 // Updated for Foundry v9 and Sequencer v2
-// https://gitlab.com/crymic/foundry-vtt-macros/-/blob/master/5e/Spells/Level%201/Magic%20Missile.js
-// Midi-qol On Use. This handles damage, so remove it from the spell card.
+// Adapting: https://gitlab.com/crymic/foundry-vtt-macros/-/blob/master/5e/Spells/Level%201/Magic%20Missile.js
+// Midi-qol On Use.
 
+// Typically, args is a length 1 array with an object with a ton of midi-qol values.
+// The variable "actor" is also defined at the top level of a macro; here it is the caster.
+// Typically, args[0].actor === actor.
+// Key arguments from midi-qol:
+// - args[0].actor: Actor5e object of the caster
+// - args[0].tokenId: ID string for the actor token
+// - args[0].itemCardId: ID string for the card of the magic missile spell in the chat
+// - args[0].targets: Array of TokenDocuments5e objects, one per each token targeted
+
+/* To test in console
+Select an actor and one or more targets
+t = canvas.tokens.controlled[0];
+targets = Array.from(game.user.targets)
+args = [ { spellLevel: 1, actor: t.actor, tokenId: t.id, targets, itemCardId: "" }]
+*/
+
+// Capitalized variables are global and may be called directly in functions below.
+
+// Values to be set by user or GM
+const COLOR = undefined; // blue, green, purple, orange, yellow, dark_red, or undefined for random
 const DAMAGE_TYPE = "force";
-const PAUSE_AFTER_DIE_ROLL = 500;
+let PAUSE_AFTER_DIE_ROLL = 0; // To give more time between die roll and animations
 
-// macro that takes caster_id, target_id, and optionally a color.
-const MM_ANIMATION = game.macros.getName("AnimateRandomMagicMissile");
-const COLOR = "Blue";
-
-async function wait(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
-}
-
-// roll combine function from Kekilla
-function combineRolls(arr) {
-    return arr.reduce((acc, val, ind) => {
-        if (ind === 0) {
-            return val;
-        } else {
-            let returnVal = new Roll(`${acc._formula} + ${val._formula}`);
-
-            returnVal.data = {};
-//             returnVal.result = [...acc.result, `+`, ...val.results];
-            returnVal.terms = [...acc.terms, `+`, ...val.terms];
-            returnVal._evaluated = true;
-            returnVal._total = acc._total + val._total;
-
-            return returnVal;
-        }
-    });
-}
-
-
+// Values set from midi-qol workflow
 if(args.length < 1) {
   ui.notifications.error("Magic Missile Macro|Arguments not found.");
   return;
 }
 console.log("Magic Missile Macro|args", args);
 
+const JB2A_COLORS = ["purple", "blue", "green", "dark_red", "orange", "yellow"];
+const NUM_MISSILES = 2 + Number(args[0].spellLevel);
+const CASTER = args[0].actor;
+const CASTER_TOKEN = canvas.tokens.get(args[0].tokenId);
+const TARGETS = args[0].targets;
+const ITEM_CARD_ID = args[0].itemCardId;
 
+if(!TARGETS.length) { return ui.notifications.error("No targets selected for magic missile spell."); }
 
-const num_missiles = 2 + Number(args[0].spellLevel);
-const actor_id = args[0].actor.id;
-const token_id = args[0].tokenId;
-const target_ids = args[0].targets.map(t => { return { id: t.id, name: t.name }; });
-const item_card_id = args[0].itemCardId;
+if(TARGETS.length === 1) {
+  // Single target; skip dialog and just fire missiles!
+  const targetsMap = new Map();
+  targetsMap.set(TARGETS[0], NUM_MISSILES);
+  await fireAtTargets(targetsMap);
 
-const actorD = game.actors.get(actor_id);
-const tokenD = canvas.tokens.get(token_id);
+} else {
+  // Multiple targets; present user with dialog to divide missiles among targets.
+  console.log("Magic Missile Macro|Multiple targets!");
+  let targetStr = "";
+  TARGETS.forEach(t => targetStr += `<tr><td>${t.name}</td><td><input type="number" id="target" class="Selection" min="0" max="${NUM_MISSILES}" name="${t.id}"></td></tr>`)
 
-console.log("Magic Missile Macro|actorD", actorD);
-console.log("Magic Missile Macro|tokenD", tokenD);
-
-//(async()=>{
-
-if(target_ids.length === 1) {
-  console.log("Magic Missile Macro|Single target.");
-
-  let the_target = canvas.tokens.get(target_ids[0].id);
-
-// avoid bug in how MidiQOL interprets damage multiplier. See https://gitlab.com/tposney/midi-qol/-/issues/347.
-//   const damageRoll = new Roll(`(1d4 +1)*${num_missiles}`).roll();
-//   console.log(`Magic Missile Macro|damageRoll`, damageRoll);
-//   new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, DAMAGE_TYPE, [the_target], damageRoll, {itemCardId: item_card_id});
-
-  const damageRoll = await (new Roll(`1d4 + 1`)).roll({async: true});
-  game.dice3d?.showForRoll(damageRoll);
-  await wait(PAUSE_AFTER_DIE_ROLL);
-
-  let damageRolls = [];
-	for(let i = 0; i < num_missiles; i++) {
-		damageRolls.push(damageRoll);
-	}
-	console.log(`Magic Missile Macro|damage rolls`, damageRolls);
-
-	const damageRollAll = combineRolls(damageRolls);
-	new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRollAll.total, DAMAGE_TYPE, [the_target], damageRollAll, {itemCardId: item_card_id});
-
-  const damage_target = `<div class="midi-qol-flex-container"><div>hits</div><div class="midi-qol-target-npc midi-qol-target-name" id="${the_target.id}"> ${the_target.name}</div><div><img src="${the_target.data.img}" width="30" height="30" style="border:0px"></div></div>`;
-  await wait(1000);
-	const chatMessage = await game.messages.get(args[0].itemCardId);
-
-	const damage_results = `<div><div class="midi-qol-nobox">${damage_target}</div></div>`;
-	const searchString =  /<div class="midi-qol-hits-display">[\s\S]*<div class="end-midi-qol-hits-display">/g;
-	const replaceString = `<div class="midi-qol-hits-display"><div class="end-midi-qol-hits-display">${damage_results}`;
-
-	let content = await duplicate(chatMessage.data.content);
-	content = await content.replace(searchString, replaceString);
-	await chatMessage.update({content: content});
-
-	if(MM_ANIMATION) {
-	  for(let i = 0; i < num_missiles; i++) {
-	    //await wait(Math.floor(Math.random() * 500)) + 100; // wait a bit to offset the missiles
-	    await MM_ANIMATION.execute(token_id, target_ids[0].id, COLOR);
-	  }
-	}
-
-} else if(target_ids.length > 1) {
-  console.log("Magic Missile Macro|Multiple targets.");
-
-  let targetList = "";
-	for (let t of target_ids) {
-		 targetList += `<tr><td>${t.name}</td><td><input type="number" id="target" class="Selection" min="0" max="${num_missiles}" name="${t.id}"></td></tr>`;
-	}
-
-	const html_header =
+  const html_dialog_header =
 `
 <script src="https://code.jquery.com/jquery-3.4.1.js"
          integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
          crossorigin="anonymous"> </script>
 `;
 
-	const html_script =
+  const html_dialog_script =
 `
 <script>
 function recalculate() {
 
-	const num_missiles = ${num_missiles};
+	let num_missiles = ${NUM_MISSILES};
 	let num_selected = 0;
 	$('input[type="number"].Selection').each(function () {
      num_selected += parseInt( $(this).val() )||0;
@@ -127,94 +94,169 @@ function recalculate() {
 
 	// update html text displaying the total number
     $('#Total').text(num_missiles -  num_selected);
-	}
+}
 
-	$('.Selection').change( function() {
-    //console.log("Selection changed.");
-    recalculate();
-  });
+$('.Selection').change( function() {
+  //console.log("Selection changed.");
+  recalculate();
+});
 
 
-  $(document).ready(function() {
-    //console.log("Document ready");
-    recalculate();
-  });
+$(document).ready(function() {
+  //console.log("Document ready");
+  recalculate();
+});
 </script>
 `;
 
+  const html_dialog_body =
+`
+<p>You have currently <span id="Total"></span> <em>magic missiles</em> remaining.</p>
+<form class="flexcol">
+  <table width="100%">
+    <tbody>
+      <tr>
+        <th>Target</th>
+        <th>Number Bolts</th>
+      </tr>${targetStr}
+    </tbody>
+  </table>
+</form>
+`;
 
-
-
-	const html_body = `<p>You have currently <span id="Total"></span> <em>magic missiles</em> remaining.</p><form class="flexcol"><table width="100%"><tbody><tr><th>Target</th><th>Number Bolts</th></tr>${targetList}</tbody></table></form>`;
-
-	const the_content = html_header + html_script + html_body;
-
-	new Dialog({
-			title: "Magic Missile Damage",
-			content: the_content,
-			buttons: {
-			one: { label: "Damage", callback: async (html) => {
-				let spentTotal = 0;
-				let selected_targets = html.find('input#target');
-				console.log("Magic Missile Macro|selected_targets", selected_targets);
-				for(let get_total of selected_targets){
-				  spentTotal += Number(get_total.value);
-			  }
-				if (spentTotal > num_missiles) return ui.notifications.error(`The spell fails, You assigned more bolts then you have.`);
-				let damage_target = [];
-				const damageRoll = new Roll(`1d4 +1`).roll();
-				console.log(`Magic Missile Macro|damageRoll`, damageRoll);
-				game.dice3d?.showForRoll(damageRoll);
-				await wait(PAUSE_AFTER_DIE_ROLL);
-
-				// cycle through each target
-				// for each, run the damage only workflow for each animation, applying the same damage each time
-				for(let selected_target of selected_targets) {
-					const damageNum = parseInt(selected_target.value)||0;
-					console.log(`Magic Missile Macro|damageNum for ${selected_target.name}`, damageNum);
-
-					if (damageNum > 0) {
-					  const target_id = selected_target.name;
-						console.log("Magic Missile Macro|target_id", target_id);
-
-						const get_target = canvas.tokens.get(target_id);
-						let damageRolls = [];
-						for(let i = 0; i < damageNum; i++) {
-						  damageRolls.push(damageRoll);
-
-						  //new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, DAMAGE_TYPE, [get_target], damageRoll, {itemCardId: args[0].itemCardId});
-						  if(MM_ANIMATION) {
-						    //await wait(Math.floor(Math.random() * 500) + 100); // wait a bit to offset the missiles
-						    await MM_ANIMATION.execute(token_id, target_id, COLOR);
-						   }
-						}
-						damage_target.push(`<div class="midi-qol-flex-container"><div>hits ${damageNum > 1 ? (" (x" + damageNum.toString() + ")") : "" }</div><div class="midi-qol-target-npc midi-qol-target-name" id="${get_target.id}"> ${get_target.name}</div><div><img src="${get_target.data.img}" width="30" height="30" style="border:0px"></div></div>`);
-
-						let damageRollAll = damageRoll;
-						if(damageRolls.length > 1) { damageRollAll = combineRolls(damageRolls); }
-						new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRollAll.total, DAMAGE_TYPE, [get_target], damageRollAll, {itemCardId: args[0].itemCardId});
-					}
-
-				}
-				let damage_list = damage_target.join('');
-				await wait(1000);
-				let damage_results = `<div><div class="midi-qol-nobox">${damage_list}</div></div>`;
-
-				const chatMessage = await game.messages.get(item_card_id);
-				const searchString =  /<div class="midi-qol-hits-display">[\s\S]*<div class="end-midi-qol-hits-display">/g;
-				const replaceString = `<div class="midi-qol-hits-display"><div class="end-midi-qol-hits-display">${damage_results}`;
-
-				let content = await duplicate(chatMessage.data.content);
-				content = await content.replace(searchString, replaceString);
-				await chatMessage.update({content: content});
-			  }
-		  }
-	  }
+  new Dialog({
+    title: "Magic Missile Damage",
+    content: html_dialog_header + html_dialog_script + html_dialog_body,
+    buttons: {
+      one: { label: "Damage", callback: multipleTargetCallback }
+    }
   }).render(true);
-
-} else {
-  console.log("Magic Missile Macro|No targets.");
-  ui.notifications.error("Magic Missile Macro|No targets selected.");
 }
 
-//})();
+/**
+ * Helper function to pause execution.
+ * @param {Number}  ms  Number of milliseconds to pause.
+ */
+async function wait(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+/**
+ * Roll combine function from Kekilla
+ * @param {Roll[]}  arr   Array of Roll objects to combine.
+ * @return {Roll} The combined roll object.
+ */
+function combineRolls(arr) {
+  if(arr.length === 0) return;
+  if(arr.length === 1) return arr[0];
+  return arr.reduce((acc, val, ind) => {
+    if(!ind) return val;
+
+    let returnVal = new Roll(`${acc._formula} + ${val._formula}`);
+    returnVal.data = {};
+    returnVal.terms = [...acc.terms, `+`, ...val.terms];
+    returnVal._evaluated = true;
+    returnVal._total = acc._total + val._total;
+    return returnVal;
+  });
+}
+
+/**
+ * Callback to process the apportioned missiles to targets.
+ * @param {Object}  html
+ */
+async function multipleTargetCallback(html) {
+  let targetsMap = new Map();
+
+  // Confirm the number of missiles do not exceed the maximum.
+  let spentTotal = 0;
+  const selected_targets = html.find('input#target');
+  for(const target of selected_targets){
+    spentTotal += Number(target.value);
+    const target_token = canvas.tokens.get(target.name); // here, target.name is the id for the token. See targetStr above.
+    targetsMap.set(target_token.document, parseInt(target.value) || 0);
+  }
+  if (spentTotal > NUM_MISSILES) { return ui.notifications.error(`The spell fails. You assigned ${spentTotal} missiles but only have ${NUM_MISSILES}!`); }
+
+  await fireAtTargets(targetsMap);
+}
+
+/**
+ * Roll the 1d4 damage die and fire missiles at one or more targets as indicated
+ * in the targets map. Update chat card accordingly.
+ * @param {Map[key: Token5e, value: Number]}  targetsMap    Map identifying number of missiles (value)
+ *                                                          to fire at each target (key).
+ */
+async function fireAtTargets(targetsMap) {
+  console.log(`Magic Missile Macro|${targetsMap.size} targets mapped.`, targetsMap);
+
+  // Roll a 1d4 + 1; possible pause for dice3d to display.
+  const damage_roll = await new Roll("1d4 +1").roll( {async: true });
+  console.log(`Magic Missile Macro|damage_roll`, damage_roll);
+  await game.dice3d?.showForRoll(damage_roll); // Roll 3d dice if available
+  await wait(PAUSE_AFTER_DIE_ROLL);
+
+  // Cycle through targets, applying the same damage to each.
+  // The targetsMap indicates how many missiles to fire at each target.
+  let damage_target = [];
+  for(const [target, num_missiles] of targetsMap) {
+    if(!num_missiles) continue; // skip if number of missiles is 0 or otherwise falsy
+    await fireMissilesAt(target, damage_roll, num_missiles)
+    damage_target.push(`<div class="midi-qol-flex-container"><div>hits ${num_missiles > 1 ? (" (x" + num_missiles.toString() + ")") : "" }</div><div class="midi-qol-target-npc midi-qol-target-name" id="${target.id}"> ${target.name}</div><div><img src="${target.data.img}" width="30" height="30" style="border:0px"></div></div>`);
+  }
+
+  const damage_list = damage_target.join('');
+  const damage_results = `<div><div class="midi-qol-nobox">${damage_list}</div></div>`;
+
+  const chatMessage = game.messages.get(ITEM_CARD_ID);
+  const searchString =  /<div class="midi-qol-hits-display">[\s\S]*<div class="end-midi-qol-hits-display">/g;
+  const replaceString = `<div class="midi-qol-hits-display"><div class="end-midi-qol-hits-display">${damage_results}`;
+
+  let content = duplicate(chatMessage.data.content);
+  content = content.replace(searchString, replaceString);
+  await chatMessage.update({ content });
+}
+
+/**
+ * Fire specified number of missiles at a given target.
+ * Then use midi-qol to update the target damage and the chat message.
+ * @param {Token5e} target          Token to fire at and apply damage.
+ * @param {Roll}    damage_roll     The damage roll to display in the chat
+ * @param {Number}  num_missiles    Number of missiles to fire
+ */
+async function fireMissilesAt(target, damage_roll, num_missiles) {
+  if(!num_missiles) return;
+
+  // Every damage roll is the same according to RAW: the previously-rolled 1d4 + 1.
+  const damage_rolls = Array(num_missiles).fill(damage_roll);
+  const damage_roll_all = combineRolls(damage_rolls);
+  animateMagicMissile(CASTER_TOKEN, target, { reps: num_missiles, color: COLOR });
+  await new MidiQOL.DamageOnlyWorkflow(CASTER, CASTER_TOKEN.document, damage_roll_all.total, DAMAGE_TYPE, [target], damage_roll_all, { itemCardId: ITEM_CARD_ID });
+}
+
+/* FYI: Midi-qol DamageOnlyWorkflow constructor:
+export class DamageOnlyWorkflow extends Workflow {
+  constructor(actor: Actor5e, token: Token, damageTotal: number, damageType: string, targets: [Token], roll: Roll,
+    options: { flavor: string, itemCardId: string, damageList: [], useOther: boolean, itemData: {}, isCritical: boolean }) {
+*/
+
+/**
+ * Use Sequencer module to animate one or more magic missiles between caster and target.
+ * @param {Token5e} caster
+ * @param {Token5e} target
+ * Optional:
+ * @param {String}  color   One of purple, blue, green,dark_red, orange, yellow.
+ *                          If not defined, chosen randomly.
+ * @param {Number} reps     Number of missiles to animate.
+ */
+function animateMagicMissile(caster, target, { color, reps = 1} = {}) {
+  color ||= JB2A_COLORS[Math.floor(Math.random() * 5)];
+  new Sequence()
+    .wait(100, 600) // offset the missiles a bit between calls
+    .effect()
+      .repeats(reps)
+      .randomOffset()
+      .delay(200, 400)   // decreased numbers: more machine-gun-like effect
+      .atLocation(caster)
+      .stretchTo(target)
+      .file(`jb2a.magic_missile.${color}`)
+    .play();
+}
